7e1688e9bd1fce9893ac89e0e59336bb27115cd9
Victor Leach <victor96@mac.com>
2015-05-30 00:22:44 +0100

    Built V0.0.1

---
+++ index.js
@@ -0,0 +1,85 @@
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var val = require('validator');
+var replace = require("replaceall");
+
+//Generate
+exports.generate = function(dingle, directory){
+	directory = path.join(process.cwd(), directory);
+	
+	//Hostname
+	var hostnames = '';
+	if (dingle.config.https.listen != '' && dingle.config.https.hostname != ''){
+		hostnames += 'exports.hostnames["HTTPS"] = "https://' + dingle.config.https.hostname + ':' + dingle.config.https.port + '";\n';
+	}
+	if (dingle.config.http.listen != '' && dingle.config.http.hostname != '' ){
+		hostnames += 'exports.hostnames["HTTP"] = "http://' + dingle.config.http.hostname + ':' + dingle.config.http.port + '";\n';
+	}
+	if (dingle.config.tcp.listen != '' && dingle.config.tcp.hostname != '' ){
+		hostnames += 'exports.hostnames["TCP"] = "tcp://' + dingle.config.tcp.hostname + ':' + dingle.config.tcp.port + '";\n';
+	}
+	if (dingle.config.udp.listen != '' && dingle.config.udp.hostname != '' ){
+		hostnames += 'exports.hostnames["UDP"] = "udp://' + dingle.config.udp.hostname + ':' + dingle.config.udp.port + '";\n';
+	}
+	
+	//Generate
+	var functions = '';
+	for (func in dingle.functions){
+		functions += generate(dingle.functions[func]) + '';
+	}
+	
+	//Make
+	var filename = path.join(directory, dingle.config.app.prefix + '.js');
+	var contents = fs.readFileSync(path.join(__dirname + '/request.js')).toString();
+	contents = replace("<hostnames>", hostnames, contents);
+	contents = replace("<functions>", functions, contents);
+	
+	//Write file
+	mkdirp(path.dirname(filename), function (error) {
+		if (error){
+			throw error;
+		}else{
+			fs.writeFile(filename, contents, function(error){
+				if (error){
+					throw error;
+				}else{
+					return;
+				}
+			});
+		}
+	});
+}
+
+//Each Call
+function generate(func){
+	
+	//Parameters
+	var param_keys = [];
+	for(var key in func.params) { param_keys.push(key); }
+	
+	//Parameters
+	var method_values = [];
+	for(var key in func.methods) { method_values.push(func.methods[key]); }
+	
+	//Generate
+	var str = '//Request\n';
+	str += 'exports.' + func.name + ' = function(' + param_keys.join(', ') + ', callback){\n';
+	str += '\n';
+	str += '	//Parameters\n';
+	str += '	var params = {};\n';
+	for (param in func.params){
+		str += '		if (' + param + ' != null){ \n';
+		str += '			if (typeof file == "object"){\n';
+		str += '				params["' + param + '"] = ' + param + ';\n';
+		str += '			}else{\n';
+		str += '				params["' + param + '"] = ' + param + '.toString();\n';
+		str += '			}\n';
+		str += '		}\n';
+	}
+	str += '	\n';
+	str += '	//Execute\n';
+	str += '	exports.sendRequest("' + func.name + '", [ "' + method_values.join('", "') + '" ], params, callback);\n';
+	str += '}\n';
+	return str;
+}
---
+++ request.js
@@ -0,0 +1,154 @@
+var http = require('http');
+var request = require('request');
+var query = require('querystring');
+var path = require('path');
+var net = require('net');
+var dgram = require("dgram");
+var url = require('url');
+
+//Hostnames
+exports.hostnames = {};
+//exports.hostnames['METHOD'] = 'URL';
+<hostnames>
+
+//Route Request
+exports.sendRequest = function(func, methods, params, callback){
+	
+	//Methods
+	for (method in methods){
+		method = methods[method];
+		
+		//Route
+		if (method == "UDP" && exports.hostnames.hasOwnProperty('UDP')){
+			return exports.sendRequestUDP(func, params, callback);
+		
+		}else if (method == "TCP" && exports.hostnames.hasOwnProperty('UDP')){
+			return exports.sendRequestTCP(func, params, callback);
+		
+		}else if (method == "POST" && (exports.hostnames.hasOwnProperty('HTTP') || exports.hostnames.hasOwnProperty('HTTPS'))){
+			return exports.sendRequestPOST(func, params, callback);
+		
+		}else if ([ "OPTIONS", "GET", "HEAD", "PUT", "PATCH", "DELETE", "TRACE", "CONNECT" ].indexOf(method) != -1
+		 	&& (exports.hostnames.hasOwnProperty('HTTP') || exports.hostnames.hasOwnProperty('HTTPS'))){
+			return exports.sendRequestHTTP(func, method, params, callback);
+		
+		}else{
+			return callback(false, 'Could not find method to call', {});
+		}
+	}
+}
+
+//HTTP Request
+exports.sendRequestHTTP = function(func, method, params, callback){
+	
+	//Method
+	if (!exports.hostnames.hasOwnProperty('HTTP') && !exports.hostnames.hasOwnProperty('HTTPS')){
+		return callback(false, 'Could not find HTTP or HTTPS hostname', {});
+	}else{
+		var hostname = exports.hostnames['HTTPS'] || exports.hostnames['HTTP'];
+	}
+	
+	//Request
+	request({
+		method: method,
+		uri: url.resolve(hostname, func + '/?' + query.stringify(params)),
+	}, function (error, response, body) {
+		
+		//Response
+		body = JSON.parse(body);
+		if(!error){
+			return callback(body.success, body.message, body.output);
+		} else {
+			return callback(false, error, body.output);
+		}
+    });
+}
+
+//HTTP POST Request
+exports.sendRequestPOST = function(func, params, callback){
+	
+	//Method
+	if (!exports.hostnames.hasOwnProperty('HTTP') && !exports.hostnames.hasOwnProperty('HTTPS')){
+		return callback(false, 'Could not find HTTP or HTTPS hostname', {});
+	}else{
+		var hostname = exports.hostnames['HTTPS'] || exports.hostnames['HTTP'];
+	}
+	
+	//Request
+	request({
+		method: "POST",
+		uri: url.resolve(hostname,func),
+		formData: params
+	}, function (error, response, body) {
+		
+		//Response
+		body = JSON.parse(body);
+		if(!error){
+			return callback(body.success, body.message, body.output);
+		} else {
+			return callback(false, error, body.output);
+		}
+    });
+}
+
+//TCP Request
+exports.sendRequestTCP = function(func, params, callback){
+	
+	//Method
+	if (!exports.hostnames.hasOwnProperty('TCP')){
+		return callback(false, 'Could not find TCP hostname', {});
+	}else{
+		var hostname = url.parse(exports.hostnames['TCP']);
+	}
+	
+	//Request
+	var socket = new net.Socket();
+	socket.connect(hostname.port, hostname.hostname, function() {
+ 		socket.write('/' + path.join(func, query.stringify(params)));
+ 		
+ 		//Response
+ 		socket.on('data', function(body) {
+ 			body = JSON.parse(body);
+			callback(body.success, body.message, body.output);
+			socket.destroy();
+		});
+	});
+}
+
+//UDP Request
+exports.sendRequestUDP = function(func, params, callback){
+	
+	//Method
+	if (!exports.hostnames.hasOwnProperty('UDP')){
+		return callback(false, 'Could not find UDP hostname', {});
+	}else{
+		var hostname = url.parse(exports.hostnames['UDP']);
+	}
+	
+	//Request
+	var socket = dgram.createSocket('udp4');
+	var message = new Buffer('/' + path.join(func, query.stringify(params)));
+	socket.send(message, 0, message.length, hostname.port, hostname.hostname, function(error, bytes) {
+		
+		//Response
+		socket.on('message', function (message, remote) {
+			body = JSON.parse(message.toString());
+			callback(body.success, body.message, body.output);
+			socket.close();
+		});
+	});
+}
+
+//Request Functions
+/*
+exports.name = function(name, callback){
+	
+	//Parameters
+	var params = {};
+	if (name != null){ params[name] = name.toString(); }
+	
+	//Execute
+	exports.sendRequest('name', [ "METHOD" ], params, callback);
+}
+*/
+<functions>
